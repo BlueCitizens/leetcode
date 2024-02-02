7584150fd8c33fc7cd62f6ee3ce953d7417170f4
BlueCitizen <bluecitizens@163.com>
2024-02-02 19:03:03 +0800

    update three sum

--- src/basic/ThreeSum.java
+++ src/basic/ThreeSum.java
@@ -42,12 +42,18 @@ public class ThreeSum {
 
 		Set<List<Integer>> s = new HashSet<>();
 		Arrays.sort(arr);
-		// 游标指向最左
+		// 游标指向最左，当前考虑的第一个数（最小数）
 		for (int i = 0; i < arr.length; i++) {
+			// 最小数大于目标，直接退出
 			if (arr[i] > target) {
 				break;
 			}
+			if (i > 0 && arr[i] == arr[i - 1]) {
+				continue;
+			}
+			// 设置左右指针，分别指向游标右侧序列的两端
 			int l = i + 1, r = arr.length - 1;
+			// 向中间移动指针
 			while (l < r) {
 				if (arr[i] + arr[l] + arr[r] < target) {
 					l++;
@@ -55,6 +61,8 @@ public class ThreeSum {
 					r--;
 				} else {
 					s.add(Arrays.asList(arr[i], arr[l], arr[r]));
+					// 跳过重复值 加速查找
+					// 如果值重复 就会一直卡在循环里直到不重复或越过边界退出
 					while (l < r && arr[l] == arr[++l]) {
 					}
 					while (l < r && arr[r] == arr[--r]) {
@@ -67,13 +75,16 @@ public class ThreeSum {
 
 	public static void main(String[] args) {
 
-		Set<List<Integer>> s = new HashSet<>();
-		List<Integer> l = new ArrayList<>();
-		s.add(Arrays.asList(1, 2, 3));
-		s.add(Arrays.asList(2, 4, 3));
-		s.add(Arrays.asList(1, 2, 3));
-		System.out.println(s.toString());
+		/*
+		 * Set<List<Integer>> s = new HashSet<>(); List<Integer> l = new ArrayList<>();
+		 * s.add(Arrays.asList(1, 2, 3)); s.add(Arrays.asList(2, 4, 3));
+		 * s.add(Arrays.asList(1, 2, 3)); System.out.println(s.toString());
+		 */
 		int[] arr = { -1, -2, -3, 4, 5, -4, 3 };
 		System.out.println(ts1(arr, 2));
+		int l = 0, r = 5;
+		/*
+		 * while (l < r && l < --r) { System.out.println(r); } System.out.println(r);
+		 */
 	}
 }
